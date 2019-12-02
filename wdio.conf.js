05d4719f6b8e117a3be7e5ba9850b3fb5692394e
andrew <andrew@textile.io>
2019-12-02 13:30:03 -0500

    try firefox image
    Signed-off-by: andrew <andrew@textile.io>

--- wdio.conf.js
+++ wdio.conf.js
@@ -69,15 +69,11 @@ exports.config = {
             // 5 instances get started at a time.
             maxInstances: 5,
             //
-            browserName: 'chrome',            
-            'goog:chromeOptions': {
-                // to run chrome headless the following flags are required
-                // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
-                args: [
-                    '--headless', 
-                    '--disable-gpu'
-                ]                
-            }
+            browserName: 'firefox',
+            'moz:firefoxOptions': {
+                // flag to activate Firefox headless mode (see https://github.com/mozilla/geckodriver/blob/master/README.md#firefox-capabilities for more details about moz:firefoxOptions)
+                args: ['-headless']
+            },
         },
         // {
         //     // maxInstances can get overwritten per capability. So if you have an in house Selenium
