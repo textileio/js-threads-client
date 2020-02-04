82cebd60e9aaae0d00132d713e05d6e326ef8279
Aaron Steele <eightysteele@gmail.com>
2020-02-04 14:05:51 -0800

    proof of concept for cloud auth
    Signed-off-by: Aaron Steele <eightysteele@gmail.com>

--- src/WriteTransaction.ts
+++ src/WriteTransaction.ts
@@ -1,3 +1,4 @@
+import { grpc } from '@improbable-eng/grpc-web'
 import * as uuid from 'uuid'
 import {
   ModelCreateRequest,
@@ -28,7 +29,9 @@ export class WriteTransaction extends Transaction<WriteTransactionRequest, Write
     startReq.setModelname(this.modelName)
     const req = new WriteTransactionRequest()
     req.setStarttransactionrequest(startReq)
-    this.client.start()
+    var metadata = new grpc.Metadata()
+    metadata.set('Authorization', 'Bearer 087e5814-b44a-4732-a15c-824bfacd4da7')
+    this.client.start(metadata)
     this.client.send(req)
   }
   /**
