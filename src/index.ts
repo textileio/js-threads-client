52bb99b7472addea0606c679903d09352dda4f02
andrew <andrew@textile.io>
2020-04-21 14:09:03 -0700

    chore(deps): remove core and use multiaddr
    Signed-off-by: andrew <andrew@textile.io>

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ import {
   GetDBInfoReply,
 } from '@textile/threads-client-grpc/api_pb'
 import nextTick from 'next-tick'
-import { Multiaddr } from '@textile/threads-core'
+import { Multiaddr } from '@textile/multiaddr'
 import { ThreadID } from '@textile/threads-id'
 import { encode, decode } from 'bs58'
 import * as pack from '../package.json'
